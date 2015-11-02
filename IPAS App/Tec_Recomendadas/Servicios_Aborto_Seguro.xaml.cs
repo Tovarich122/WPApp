630bb13b7dafa925bdf28428713bb7179ce3a8cc
OsorioOmar <osorioctrlfreak@gmail.com>
2015-11-02 11:42:17 -0600

    Versión 0.17
    Se agregan modificaciones a Tecnologías Recomendadas

---
+++ IPAS App/Tec_Recomendadas/Servicios_Aborto_Seguro.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace IPAS_App
+{
+    public partial class Servicios_Aborto_Seguro : PhoneApplicationPage
+    {
+        public Servicios_Aborto_Seguro()
+        {
+            InitializeComponent();
+        }
+    }
+}
